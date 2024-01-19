dfa3255fa61b4fc2996ec6ce1e140d6d3e3a0f48
willemhelf <sophie.helf@gmail.com>
2024-01-19 14:57:57 -0500

    integrating socket.io

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 import express from 'express';
 import { createServer } from 'node:http';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { Server } from 'socket.io'
 
 const app = express();
 // initialize app to be function handler you cna supply to HTTP server
 const server = createServer(app);
+const io = new Server(server)
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(join(__dirname, 'index.html'))
 });
-// define route handler (/) that gets called when website home is hit
+// load html file on request
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+  });
 
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
