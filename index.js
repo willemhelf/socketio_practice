ec5f2739fd2a23c8439bc9b9deb64da5adb4deb2
willemhelf <sophie.helf@gmail.com>
2024-01-19 15:29:50 -0500

    wrapping up

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ import { dirname, join } from 'node:path';
 import { Server } from 'socket.io'
 import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
+import { availableParallelism } from 'node:os';
+import cluster from 'node:cluster';
+import { createAdapter, setupPrimary} from '@socket.io/cluster-adapter';
 
 const db = await open({
     filename: 'chat.db',
@@ -19,12 +22,22 @@ await db.exec(`
     )
 `)
 
-const app = express();
-// initialize app to be function handler you cna supply to HTTP server
-const server = createServer(app);
-const io = new Server(server, {
-    connectionStateRecovery: {} // recover chat after disconnect
-})
+if (cluster.isPrimary) {
+    const numCPUs = availableParallelism()
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork({
+            PORT: 3000+i
+        })
+    }
+    setupPrimary()
+} else {
+    const app = express();
+    // initialize app to be function handler you cna supply to HTTP server
+    const server = createServer(app);
+    const io = new Server(server, {
+        connectionStateRecovery: {} // recover chat after disconnect
+    })
+}
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -33,16 +46,23 @@ app.get('/', (req, res) => {
 });
 // load html file on request
 
-io.on('connection', (socket) => {
-    socket.on('chat message', async (msg) => {
+io.on('connection', async (socket) => {
+  socket.on('chat message', async (msg, clientOffset, callback) => {
     let result;
     try {
-        result = await db.run('INSERT INTO meessages (content) VALUES (?)', msg)
+      result = await db.run('INSERT INTO messages (content, client_offset) VALUES (?, ?)', msg, clientOffset);
     } catch (e) {
-        return;
+      if (e.errno === 19 /* SQLITE_CONSTRAINT */ ) {
+        // the message was already inserted, so we notify the client
+        callback();
+      } else {
+        // nothing to do, just let the client retry
+      }
+      return;
     }
-      io.emit('chat message', msg);
-    });
+    io.emit('chat message', msg, result.lastID);
+    // acknowledge the event
+    callback();
   });
 
   if(!socket.recovered) {
@@ -56,6 +76,7 @@ io.on('connection', (socket) => {
         console.log("ruh roh!")
     }
   }
+})
 
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
